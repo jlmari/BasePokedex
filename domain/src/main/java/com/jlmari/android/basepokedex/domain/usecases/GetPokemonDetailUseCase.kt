e18fb0036991973963e86be1c5eebea131db1a7f
jlmari <jlmari@visual-engin.com>
2022-12-23 16:57:45 +0100

    POK-6: make async calls inside IO dispatcher to avoid overloads in main thread

--- domain/src/main/java/com/jlmari/android/basepokedex/domain/usecases/GetPokemonDetailUseCase.kt
+++ domain/src/main/java/com/jlmari/android/basepokedex/domain/usecases/GetPokemonDetailUseCase.kt
@@ -1,9 +1,15 @@
 package com.jlmari.android.basepokedex.domain.usecases
 
+import com.jlmari.android.basepokedex.domain.dispatchers.AppDispatchers
 import com.jlmari.android.basepokedex.domain.repositories.PokeRepository
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class GetPokemonDetailUseCase @Inject constructor(private val repository: PokeRepository) {
+class GetPokemonDetailUseCase @Inject constructor(
+    private val appDispatchers: AppDispatchers,
+    private val repository: PokeRepository
+) {
 
-    suspend operator fun invoke(id: Int) = repository.getPokemonDetail(id)
+    suspend operator fun invoke(id: Int) =
+        withContext(appDispatchers.io) { repository.getPokemonDetail(id) }
 }
